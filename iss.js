7a09e011b125a08da36bfe74a858398f4f81055d
Darrenni97 <Darrenni_@hotmail.com>
2019-09-26 16:35:21 -0700

    implemented request to get ip address

--- iss.js
+++ iss.js
@@ -11,10 +11,13 @@ const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
 
   request('https://api.ipify.org/?format=json', (error, response, body) => {
-    console.log(body);
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, data.ip);
+    }
   });
 }
-
-fetchMyIP();
 
 module.exports = { fetchMyIP };
