86525ee250770447ec49ddbbc463cc48a5e9a6a0
Darrenni97 <Darrenni_@hotmail.com>
2019-09-26 16:39:34 -0700

    no response function and linted

--- iss.js
+++ iss.js
@@ -7,17 +7,23 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
 
   request('https://api.ipify.org/?format=json', (error, response, body) => {
     const data = JSON.parse(body);
     if (error) {
       callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
     } else {
       callback(null, data.ip);
     }
   });
-}
+};
 
 module.exports = { fetchMyIP };
