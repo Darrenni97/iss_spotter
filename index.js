11be31cfd6dbf33b13d23cc6d16e39d7d2d48902
Darrenni97 <Darrenni_@hotmail.com>
2019-09-26 18:12:11 -0700

    implemented flyovertimes function

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+
 
 fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned IP:' , ip);
+  fetchCoordsByIP(ip, (error, coords) => {
+    fetchISSFlyOverTimes(coords, (error, data) => {
+      if (error) {
+        console.log(error);
+      } else console.log(data);
+    })
+  });
 });
+
+
+
