06e5406fb447940da3b67e9179850527c403c969
Darrenni97 <Darrenni_@hotmail.com>
2019-09-26 19:06:12 -0700

    implmented nextISSTimes based on coords

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
 const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 
 fetchMyIP((error, ip) => {
   fetchCoordsByIP(ip, (error, coords) => {
     fetchISSFlyOverTimes(coords, (error, data) => {
-      if (error) {
-        console.log(error);
-      } else console.log(data);
+      const printPassTimes = function(passTimes) {
+        for (const pass of passTimes) {
+          const datetime = new Date(0);
+          datetime.setUTCSeconds(pass.risetime);
+          const duration = pass.duration;
+          console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+        }
+      };
+      nextISSTimesForMyLocation((error, passTimes) => {
+        if (error) {
+          return console.log("It didn't work!", error);
+        }
+        // success, print out the deets!
+        printPassTimes(passTimes);
+      });
     })
   });
 });
